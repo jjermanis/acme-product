5be09e4e8e5bfa26a879d5c34b26cfe59021afa2
John Jermanis <jermanis@yahoo.com>
2018-01-12 13:58:05 -0800

    Custom pipe, component style, interface

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: 'product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ["./product-list.component.css"],
 })
-export class ProductListComponent {
+export class ProductListComponent 
+    implements OnInit {
     pageTitle: string="Product List";
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = "ice";
-    products: any[]=
+    products: IProduct[]=
     [
         {
             imageUrl:"https://peopledotcom.files.wordpress.com/2016/05/mc-hammer-02-435.jpg",
@@ -32,5 +35,9 @@ export class ProductListComponent {
     ];
     toggleImage():void {
        this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log("In OnInit")
     }
 }
---
+++ src/app/products/product.ts
@@ -0,0 +1,8 @@
+export interface IProduct {
+    imageUrl: string;
+    name: string;
+    code: string;
+    available: number;
+    price: number;
+    rating: number;
+}
